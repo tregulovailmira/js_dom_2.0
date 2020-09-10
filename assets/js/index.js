a1dc1eeb9f084c58799d4f344697e919681a0601
Ilmira <tregulovailmira@gmail.com>
2020-09-10 15:27:59 +0300

    Create calculating sphere volume (task 1)

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,22 @@
 'use strict'
+
+/*1.Сделать два инпута: в один вводить параметр(число), во втором показывать объем шара при заданном параметре.*/
+
+const divFirstTask = document.getElementById('firstTask');
+
+const input = document.createElement('input');
+input.type = 'number';
+divFirstTask.appendChild(document.createTextNode('Enter radius, m:'));
+divFirstTask.appendChild(input);
+
+let resultInput = document.createElement('input');
+resultInput.readOnly = true;
+divFirstTask.appendChild(document.createTextNode('Sphere volume, m3:'));
+divFirstTask.appendChild(resultInput);
+
+const calculateButton = document.createElement('button');
+calculateButton.textContent = 'Calculate';
+divFirstTask.appendChild(calculateButton);
+calculateButton.addEventListener('click', (event) => {
+    return resultInput.value = 4/3 * Math.PI * input.value ** 3;
+});
